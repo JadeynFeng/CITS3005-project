09844efd6803d462c1a8f7e14c33be5a87a7eea0
LisonLau <allisonlhm@gmail.com>
2023-09-30 02:54:38 +0800

    Read majors json file into rdf

--- project.py
+++ project.py
@@ -11,10 +11,13 @@ schema_ns = Namespace("http://schema.org/")
 import json
 
 with open("units.json", "r") as json_file:
-    data = json.load(json_file)
+    units_data = json.load(json_file)
+    
+with open("majors.json", "r") as json_file:
+    majors_data = json.load(json_file)
 
-# Iterate through the JSON data and create RDF triples
-for unit_code, unit_data in data.items():
+# Iterate through the units JSON data and create RDF triples
+for unit_code, unit_data in units_data.items():
     unit_uri = URIRef(f"http://example.org/units/{unit_code}")
 
     # Add RDF triples for each unit attribute
@@ -61,5 +64,34 @@ for unit_code, unit_data in data.items():
     if "note" in unit_data:
         g.add((unit_uri, schema_ns.note, Literal(unit_data["note"])))
 
+
+# Iterate through the majors JSON data and create RDF triples
+for major_code, major_data in majors_data.items():
+    major_uri = URIRef(f"http://example.org/majors/{major_code}")
+
+    # Add RDF triples for each major attribute
+    g.add((major_uri, RDF.type, schema_ns.Course))
+    g.add((major_uri, schema_ns.code, Literal(major_code)))
+    g.add((major_uri, schema_ns.title, Literal(major_data["title"])))
+    g.add((major_uri, schema_ns.school, Literal(major_data["school"])))
+    g.add((major_uri, schema_ns.board_of_examiners, Literal(major_data["board_of_examiners"])))
+    g.add((major_uri, schema_ns.delivery_mode, Literal(major_data["delivery_mode"])))
+    g.add((major_uri, schema_ns.description, Literal(major_data["description"])))
+    
+    for outcome in major_data["outcomes"]:
+        g.add((major_uri, schema_ns.outcomes, Literal(outcome)))
+    
+    if "prerequisites" in major_data:
+        g.add((major_uri, schema_ns.prerequisites, Literal(major_data["prerequisites"])))
+    
+    for course in major_data["courses"]:
+        g.add((major_uri, schema_ns.courses, Literal(course)))
+        
+    for bridge in major_data["bridging"]:
+        g.add((major_uri, schema_ns.bridging, Literal(bridge)))
+        
+    for unit in major_data["units"]:
+        g.add((major_uri, schema_ns.units, Literal(unit)))
+
 # Serialize the RDF graph to a file (e.g., in Turtle format)
-g.serialize("units.rdf", format="turtle")
+g.serialize("project.rdf", format="turtle")
