441de978ad56ebd226897f2af9a1e7ca01bbb076
LisonLau <allisonlhm@gmail.com>
2023-10-15 15:37:43 +0800

    Update ontology.py

--- ontology.py
+++ ontology.py
@@ -1,22 +1,25 @@
 from owlready2 import *
 onto_path.append(".")
-onto = get_ontology("http://uwabookofknowledge.org/onto.owl#")
+onto = get_ontology("http://uwabookofknowledge.org/ontology.owl#")
 
 with onto:
+    # Define classes
     class Unit(Thing): pass
-    class UnitCode(Unit): pass # a subclass of Unit
+    class UnitCode(Unit): pass
     class UnitTitle(Unit): pass
 
     class Major(Thing): pass
     class MajorCode(Major): pass
     class MajorTitle(Major): pass
 
-    class Prereq(Thing): pass
-    class OrReq(Prereq): pass
+    class Prerequisite(Thing): pass
+    class OrReq(Prerequisite): pass
 
+    class Outcome(Thing): pass
+    class Assessment(Thing): pass
     class Contact(Thing): pass
 
-# properties ======================================
+    # Define object properties
     class has_unitcode(ObjectProperty, FunctionalProperty): 
         domain = [Unit]
         range = [UnitCode]
@@ -28,12 +31,27 @@ with onto:
     class has_unitlevel(DataProperty): 
         domain = [Unit]
         range = [int]
+        
+    class has_prerequisite(DataProperty):
+        domain = [Unit]
+        range = [Prerequisite]
+        
+    class has_prerequisite_text(ObjectProperty):
+        domain = [Unit]
+        range = [str]
+        
+    class has_outcome(ObjectProperty):
+        domain = [Unit]
+        range = [Outcome]
+
+    class has_assessment(ObjectProperty):
+        domain = [Unit]
+        range = [Assessment]
     
     class is_part_of_major(ObjectProperty): 
         domain = [Unit]
         range = [Major]
             
-
     class has_majorcode(ObjectProperty, FunctionalProperty): 
         domain = [Major]
         range = [MajorCode]
@@ -46,6 +64,42 @@ with onto:
         domain = [Major]
         range = [Unit]
         inverse_property = is_part_of_major
+        
+    # Define rules
+    class PrerequisiteOfPrerequisite(Prerequisite >> Prerequisite, FunctionalProperty):
+        pass
+    class OutcomeOfCoreUnit(Outcome >> Major, FunctionalProperty):
+        pass
+    # class PrerequisiteTextOfCoreUnit(Prerequisite >> str, FunctionalProperty):
+    #     pass
 
-onto.save()
+    # Add data to Knowledge Graph
+    # Add Unit
+    new_unit = Unit("CITS1111")
+    new_unit.has_unitcode = UnitCode("CITS1111")
+    new_unit.has_unittitle = UnitTitle("Artificial Intelligence")
+    new_unit.has_unitlevel.append(1)
+    new_unit.has_outcome.append(Outcome("Learn about AI"))
+    new_unit.has_assessment.append(Assessment("Exams"))
+    
+    # Add Major
+    major = Major("CS")
+    major.has_majorcode = MajorCode("CS")
+    major.has_majortitle = MajorTitle("Computer Science")
+    major.contains_unit.append(new_unit)
+    new_unit.is_part_of_major.append(major)
+    
+    # Update data in Knowledge Graph
+    update_unit = onto.search_one(iri = "*CITS1111")
+    update_unit.has_unitlevel.append(3)
+    update_unit.has_outcome.append(Outcome("Learn about AI and ML"))
+    update_unit.has_assessment.append(Assessment("Exams and assignments"))
+    
+    # Delete data from Knowledge Graph
+    delete_unit = onto.search_one(iri = "*CITS1111")
+    destroy_entity(delete_unit)
+
+onto.load()
+sync_reasoner()
+
 onto.save(file = "ontology.owl", format = "rdfxml")
